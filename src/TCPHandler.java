77728b7f163e7c9d6191e978b9c2f58ed87c4629
PeterZegarek <petrzegarek@gmail.com>
2024-04-13 11:44:06 -0400

    Network setup

--- src/TCPHandler.java
+++ src/TCPHandler.java
@@ -1,17 +1,26 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPHandler implements Runnable
 {
+
+    // whether client or server
+    private boolean client = false;
     //TCP connection
     private Socket socket;
     private DataOutputStream out;
     private DataInputStream in;
 
+    // server data
+    private ServerSocket serverSocket;
+
+    // spun off by client
     public TCPHandler(Socket socket)
     {
+        this.client = true;
         this.socket = socket;
         try
         {
@@ -23,9 +32,47 @@ public class TCPHandler implements Runnable
         }
     }
 
+    // server starts off this one
+    public TCPHandler(ServerSocket socket){
+        this.client = false;
+        this.serverSocket = socket;
+        // accept client, and update input/output streams
+        try {
+            System.out.println("Hello");
+            Socket temp = serverSocket.accept();
+            out = new DataOutputStream(temp.getOutputStream());
+            in = new DataInputStream(temp.getInputStream());
+            // TCPHandler ch = new TCPHandler(temp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendHello(){
+        try {
+            out.writeInt(1);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    // the thread spins off and does this
     @Override
     public void run()
     {
+        // if client
+        // if (client){
+        //     while (true){
 
+        //     }
+        // }
+        // // if server
+        // else {
+        //     while (true){
+
+        //     }
+        // }
     }
 }
