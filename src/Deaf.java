75ed870b73b6598f6d3f35a5d7d2e5566a9c62df
Chris Anzilotti <anzilottichr@gmail.com>
2024-04-13 10:25:04 -0400

    Basic TCP

--- src/Deaf.java
+++ src/Deaf.java
@@ -1,3 +1,19 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Deaf
 {
+    //TCP
+    private Socket socket;
+    private TCPHandler tcpHandler;
+
+
+    public Deaf(Socket socket)
+    {
+        this.socket = socket;
+
+        tcpHandler = new TCPHandler(socket);
+    }
 }
